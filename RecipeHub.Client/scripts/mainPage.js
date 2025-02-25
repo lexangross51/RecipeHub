b79a0e393bb0956222f8b6063b75abeb80ed26ce
Alex PRO <lexangross51@gmail.com>
2025-02-25 22:35:26 +0700

    Добавил сортировку и добавил немного инфы на страницу с детальным описанием рецепта

--- RecipeHub.Client/scripts/mainPage.js
+++ RecipeHub.Client/scripts/mainPage.js
@@ -2,27 +2,46 @@ const api = 'https://localhost:7244/api/v1/';
 
 document.addEventListener("DOMContentLoaded", () => {
     attachEventListeners();
-    getRecipes();
+    getRecipes({});
 });
 
 function attachEventListeners() {
     const search = document.getElementById('input-search');
     search.addEventListener('input', async (e) => {
         const inputName = e.target.value;
-        await getRecipes(inputName);
+        await getRecipes({ searchName: inputName });
+    })
+
+    const sortSelect = document.getElementById('sort-field-select');
+    sortSelect.addEventListener('change', async function() {
+        const selectedSort = this.value;
+        const options = selectedSort.split('-');
+        const name = document.getElementById('input-search').value;
+    
+        await getRecipes({ searchName: name, sortBy: [{
+            field: options[0],
+            order: options[1]
+        }]});
     })
 }
 
-async function getRecipes(searchName = '', take = 4) {
+async function getRecipes({ searchName = '', take = 4, sortBy }) {
     let link = api + `recipes?take=${take}`;
 
     if (searchName) {
         link += `&name=${searchName}`;
     }
 
-    console.log(`Получаем список рецептов по адресу: ${link}`);
-    document.getElementById('recipes-list').children.cle
+    if (sortBy) {
+        for (let i = 0; i < sortBy.length; i++) {
+            const sort = sortBy[i];
 
+            if (!sort) continue;
+            
+            link += `&sortBy[${i}].field=${sort.field}&sortBy[${i}].order=${sort.order}`
+        }
+    }
+    
     fetch(link, {
         method: 'GET'
     })
@@ -43,24 +62,17 @@ async function getRecipes(searchName = '', take = 4) {
     });
 }
 
-async function getRecipe(id) {
-    fetch(api + `recipes/${id}`)
-    .then(response => response.json())
-    .then(recipe => {
-        console.log(`Полученный рецепт: ${recipe.id}`);
-    })
-    .catch(error => {
-        console.error(`Произошла ошибка: ${error.message}`);
-    })
-}
-
 function createRecipeCard(recipe) {
     const recipeTemplate = document.getElementById('recipe-item-template');
     const ingredientTemplate = document.getElementById('ingredient-item-template');
     const newRecipe = recipeTemplate.content.cloneNode(true).firstElementChild;
+    const recipeImage = newRecipe.querySelector('.recipe-image');
+    recipeImage.addEventListener('error', function() {
+        this.src = '../assets/images/none.jpg';
+    });
 
     newRecipe.dataset.id = recipe.id;
-    newRecipe.querySelector('.recipe-image').src = recipe.imageUrl
+    recipeImage.src = recipe.imageUrl;
     newRecipe.querySelector('h3').innerText = recipe.name;
     newRecipe.querySelector('p').innerText = `Время приготовления: ${recipe.cookingTime}`;
     const ingredientList = newRecipe.querySelector('.ingredient-list');
@@ -75,7 +87,7 @@ function createRecipeCard(recipe) {
     return newRecipe;
 }
 
-async function selectRecipe() {
+function selectRecipe() {
     // this - элемент списка (li)
     const recipeId = this.dataset.id;
     window.location.href = `recipeDetails.html?id=${recipeId}`;
--- RecipeHub.Client/scripts/recipeDetails.js
+++ RecipeHub.Client/scripts/recipeDetails.js
@@ -2,7 +2,7 @@ const api = 'https://localhost:7244/api/v1/';
 
 document.addEventListener('DOMContentLoaded', async () => {
     var queryParams = new URLSearchParams(window.location.search);
-    const recipeId = queryParams.get('id');
+    const recipeId = queryParams.get('id')/*'91367195-a34d-4023-a776-bf3f1c5a7ecd'*/;
     await getRecipe(recipeId);
 });
 
@@ -11,14 +11,33 @@ async function getRecipe(id) {
     .then(response => response.json())
     .then(recipe => {
         fillElementsWithRecipeData(recipe);
-    })
+    });
 }
 
 function fillElementsWithRecipeData(recipe) {
-    document.getElementById('recipe-image').src = recipe.imageUrl;
+    console.log(recipe);
+    
+    const recipeImage = document.getElementById('recipe-image');
+    setDefaultImageWhenError(recipeImage, recipe.imageUrl);
+    
     document.getElementById('recipe-name').innerText = recipe.name;
     const ingredientList = document.getElementById('ingredients-list');
     const stepsList = document.getElementById('steps-list');
+    const recipeDesc = document.getElementById('recipe-description-block');
+    const desc = recipeDesc.querySelector('#recipe-description');
+    const cookTime = recipeDesc.querySelector('#recipe-cooking-time');
+    
+    if (!desc && !cookTime) {
+        recipeDesc.display = 'none';
+    } else {
+        if (recipe.description) {
+            desc.innerText = recipe.description;
+        }
+
+        if (recipe.cookingTime) {
+            cookTime.innerText = `Время приготовления: ${recipe.cookingTime}`;
+        }
+    }
 
     // добавляем инфу об ингредиентах
     for (const ingredient of recipe.ingredients) {
@@ -47,7 +66,6 @@ function createIngredientItem(ingredient) {
 function createRecipeStepItem(step, isOne = false) {
     const stepItemTemplate = document.getElementById('step-item-template');
     const stepElem = stepItemTemplate.content.cloneNode(true).firstElementChild;
-
     const stepNumber = stepElem.querySelector('.step-number');
 
     if (isOne) {
@@ -57,14 +75,21 @@ function createRecipeStepItem(step, isOne = false) {
     }
 
     stepElem.querySelector('.step-description').innerText = step.description;
-    console.log(stepElem);
-    if (step.imageUrl) {
-        stepElem.querySelector('.step-image').src = step.imageUrl;
-    }
 
-    if (isOne) {
-        stepElem.queryParams
+    if (step.imageUrl) {
+        const stepImage = stepElem.querySelector('.step-image');
+        setDefaultImageWhenError(stepImage, step.imageUrl);
     }
 
     return stepElem;
+}
+
+function setDefaultImageWhenError(image, url) {
+    image.addEventListener('error', function () {
+        this.src = '../assets/images/none.jpg';
+    });
+
+    if (url) {
+        image.src = url;
+    }
 }
